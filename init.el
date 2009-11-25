2731860cbf0794978c9eb2a01d2e96bcce1d0911
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2009-11-25 23:44:26 +0530

    Automatically recompile .emacs when it is saved

--- init.el
+++ init.el
@@ -7,3 +7,17 @@
 (load-file "~/.emacs.d/load/prefs.el")
 (load-file "~/.emacs.d/load/snippets.el")
 (load-file "~/.emacs.d/load/ido-smex.el")  ;; Should be loaded last.
+
+(defun byte-compile-user-init-file ()
+  (let ((byte-compile-warnings '(unresolved)))
+        ;; in case compilation fails, don't leave the old .elc around:
+        (when (file-exists-p (concat user-init-file ".elc"))
+          (delete-file (concat user-init-file ".elc")))
+        (byte-compile-file user-init-file)
+        ;; (message "%s compiled" user-init-file)
+        ))
+(defun my-emacs-lisp-mode-hook ()
+  (when (equal buffer-file-name user-init-file)
+    (add-hook 'after-save-hook 'byte-compile-user-init-file t t)))
+;; (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-hook)
