017f64f6ab38fd3f48b16a1fc238e571bed238bc
Yesudeep Mangalapilly <yesudeep@happychickoo.com>
2009-11-26 01:35:37 +0530

    configure anything and add find-recursive

--- load/completion.el
+++ load/completion.el
@@ -2,3 +2,10 @@
 
 (add-to-list 'load-path "~/.emacs.d/plugins/nxhtml/util")
 (require 'tabkey2)
+
+;; Anything
+(add-to-list 'load-path "~/.emacs.d/plugins/anything-config")
+(require 'anything)
+(require 'anything-config)
+(require 'anything-extension)
+
