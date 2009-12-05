00db886a9f29684ddedeca1e6b705e7a234607a4
Yesudeep Mangalapilly <yesudeep@meebo.(none)>
2009-12-05 23:32:32 +0530

    clean up completion.el

--- load/completion.el
+++ load/completion.el
@@ -18,7 +18,6 @@
 ;; Using Yasnippet
 ;; http://code.google.com/p/yasnippet/
 
-
 (require 'yasnippet)
 ;; Don't map TAB to yasnippet, we want to trigger it using auto-complete.el
 (setq yas/trigger-key (kbd "C-c <kp-multiply>"))
@@ -33,7 +32,6 @@
 
 ;; Configuration
 (global-auto-complete-mode t)
-(setq-default ac-sources '(ac-source-words-in-same-mode-buffers))
 (setq ac-auto-start 2)
 (setq ac-dwim t)
 
@@ -42,10 +40,77 @@
 (define-key ac-completing-map "\M-p" 'ac-previous)
 (define-key ac-mode-map (kbd "M-TAB") 'auto-complete)
 
+;; The common sources for all modes.
+(custom-set-variables
+ '(ac-sources
+   '(ac-source-abbrev
+     ac-source-words-in-buffer
+     ac-source-words-in-same-mode-buffers
+     ac-source-files-in-current-dir
+     ac-source-filename)))
+
+;; Autocomplete will be enabled for all these modes.
+(setq-default ac-modes
+      '(ada-mode
+        asm-mode
+        c++-mode
+        c-mode
+        cc-mode
+        cperl-mode
+        css-mode
+        css-mode
+        ecmascript-mode
+        emacs-lisp-mode
+        emms-tag-editor-mode
+        eshell-mode
+        f90-mode
+        fortran-mode
+        haskell-mode
+        java-mode
+        javascript-mode
+        lisp-interaction-mode
+        lisp-mode
+        literate-haskell-mode
+        makefile-mode
+        org-mode
+        perl-mode
+        php-mode
+        python-mode
+        ruby-mode
+        sass-mode
+        scheme-mode
+        sgml-mode
+        sh-mode
+        xml-mode))
+;; (add-to-list 'ac-trigger-commands 'org-self-insert-command) ; if you want enable auto-complete at org-mode, uncomment this line
+
+
 ;; Mode hooks
-(add-hook 'python-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-symbols 'ac-source-filename 'ac-source-files-in-current-dir)))
-(add-hook 'emacs-lisp-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-symbols)))
-(add-hook 'auto-complete-mode-hook (lambda () (add-to-list 'ac-sources 'ac-source-filename)))
+(add-hook 'eshell-mode-hook
+          (lambda () (add-to-list 'ac-sources
+                                  '(ac-source-abbrev
+                                    ac-source-files-in-current-dir
+                                    ac-source-words-in-buffer))))
+(add-hook 'python-mode-hook
+          (lambda () (add-to-list 'ac-sources
+                                  '(ac-source-symbols
+                                    ac-source-filename
+                                    ac-source-yasnippet
+                                    ac-source-ropemacs
+                                    ac-source-files-in-current-dir))))
+(add-hook 'emacs-lisp-mode-hook
+          (lambda () (add-to-list 'ac-sources
+                                  'ac-source-symbols)))
+(add-hook 'auto-complete-mode-hook
+          (lambda () (add-to-list 'ac-sources
+                                  'ac-source-filename)))
+(add-hook 'css-mode-hook
+          (lambda() (add-to-list 'ac-sources
+                                 '(ac-source-symbols
+                                   ac-source-files-in-current-dir
+                                   ac-source-filename
+                                   ac-source-css-keywords))))
+
 
 ;; ***************************************************************************
 ;; Py-complete, ropemacs, and pymacs
@@ -63,5 +128,3 @@
 (autoload 'pymacs-eval "pymacs" nil t)
 (autoload 'pymacs-apply "pymacs")
 (autoload 'pymacs-call "pymacs")
-
-
