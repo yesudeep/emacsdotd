2d0d10673dc0c04cf961fdc697a71e674f056441
Yesudeep Mangalapilly <yesudeep@mia.local>
2009-12-08 00:24:27 +0530

    Add more major modes as candidates for automatic indentation on paste

--- load/prefs.el
+++ load/prefs.el
@@ -34,13 +34,22 @@
 (dolist (command '(yank yank-pop))
   (eval `(defadvice ,command (after indent-region activate)
            (and (not current-prefix-arg)
-                (member major-mode '(emacs-lisp-mode lisp-mode
-                                                     clojure-mode    scheme-mode
-                                                     haskell-mode    ruby-mode
-                                                     rspec-mode      python-mode
-                                                     c-mode          c++-mode
-                                                     objc-mode       latex-mode
-                                                     plain-tex-mode))
+                (member major-mode '(emacs-lisp-mode
+                                     lisp-mode
+                                     clojure-mode
+                                     scheme-mode
+                                     haskell-mode
+                                     ruby-mode
+                                     rspec-mode
+                                     python-mode
+                                     c-mode
+                                     c++-mode
+                                     objc-mode
+                                     latex-mode
+                                     plain-tex-mode
+                                     html-mode
+                                     xml-mode
+                                     yaml-mode))
                 (let ((mark-even-if-inactive transient-mark-mode))
                   (indent-region (region-beginning) (region-end) nil))))))
 
