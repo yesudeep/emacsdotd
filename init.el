3f6e838b0b2a9470e721c282fca18d271bc3c221
Yesudeep Mangalapilly <yesudeep@mia.local>
2009-12-08 16:53:24 +0530

    clean up all the hardcoded paths and some code as well

--- init.el
+++ init.el
@@ -1,72 +1,106 @@
 ;; URL: http://github.com/yesudeep/emacsdotd
 
-;; Python path and version.
-(setenv "PYTHONPATH" (concat (getenv "PYTHONPATH")
-                             ":" (expand-file-name "~/.emacs.d/plugins/pymacs")
-                             ":" (expand-file-name "~/.emacs.d/modes/python-mode")
-                             ":" (expand-file-name "~/.emacs.d/plugins/ropemode")
-                             ))
+;; ***************************************************************************
+;; Elisp modules root directory
+;; ----------------------------
+;;
 
-;; Add these directories to the load path.
-(add-to-list 'load-path "~/.emacs.d/")
-(add-to-list 'load-path "~/.emacs.d/plugins")
-(add-to-list 'load-path "~/.emacs.d/load")
-(add-to-list 'load-path "~/.emacs.d/modes")
-(add-to-list 'load-path "~/.emacs.d/modes/python-mode")
-(progn (cd "~/.emacs.d")
-       (normal-top-level-add-subdirs-to-load-path))
-(progn (cd "~/.emacs.d/color-themes")
-       (normal-top-level-add-subdirs-to-load-path))
-(progn (cd "~/.emacs.d/plugins")
-       (normal-top-level-add-subdirs-to-load-path))
-(progn (cd "~/.emacs.d/modes")
-       (normal-top-level-add-subdirs-to-load-path))
+(setq emacsdotd-root-dir (file-name-directory
+                          (or (buffer-file-name) load-file-name)))
+
 
 ;; ***************************************************************************
-;; Custom hooks
+;; Python path and version.
+;; ------------------------
+;;
+
+(setenv "PYTHONPATH" (concat (getenv "PYTHONPATH")
+                             ":" (expand-file-name (concat emacsdotd-root-dir "/plugins/pymacs"))
+                             ":" (expand-file-name (concat emacsdotd-root-dir "/modes/python-mode"))
+                             ":" (expand-file-name (concat emacsdotd-root-dir "/plugins/ropemode"))
+                             ))
+
+;; ***************************************************************************
+;; Load-path
+;; ---------
+;;
+
+(add-to-list 'load-path emacsdotd-root-dir)
+
+;; Add all these subdirectories.
+(progn (cd emacsdotd-root-dir)
+       (normal-top-level-add-subdirs-to-load-path))
+(progn (cd (concat emacsdotd-root-dir "/color-themes"))
+       (normal-top-level-add-subdirs-to-load-path))
+(progn (cd (concat emacsdotd-root-dir "/plugins"))
+       (normal-top-level-add-subdirs-to-load-path))
+(progn (cd (concat emacsdotd-root-dir "/modes"))
+       (normal-top-level-add-subdirs-to-load-path))
+
+
+;; ***************************************************************************
+;; Load these preferences
+;; ----------------------
+;;
+;; Please load the hc-ido-smex library last.
+
+(load-library "hc-colors")
+(load-library "hc-modes")
+(load-library "hc-prefs")
+(load-library "hc-typing-pairs")
+(load-library "hc-completion")
+(load-library "hc-misc")
+(load-library "hc-ido-smex")
+
+
+;; ***************************************************************************
+;; Custom hooks for the window frame
+;; ---------------------------------
 ;; Two new separate hooks for the creation of window-system and tty (console) frames.
 ;; This is used to run different code for console and window-system.
 ;; See colors.el for example.
+;;
+
 (defvar after-make-console-frame-hooks '()
-"Hooks to run after creating a new TTY frame")
+  "Hooks to run after creating a new TTY frame")
+
 (defvar after-make-window-system-frame-hooks '()
-"Hooks to run after creating a new window-system frame")
+  "Hooks to run after creating a new window-system frame")
 
 (defun run-after-make-frame-hooks (frame)
-"Selectively run either `after-make-console-frame-hooks' or
+  "Selectively run either `after-make-console-frame-hooks' or
 `after-make-window-system-frame-hooks'"
-(select-frame frame)
-(run-hooks (if window-system
-'after-make-window-system-frame-hooks
-'after-make-console-frame-hooks)))
+  (select-frame frame)
+  (run-hooks (if window-system
+                 'after-make-window-system-frame-hooks
+               'after-make-console-frame-hooks)))
 
-(add-hook 'after-make-frame-functions 'run-after-make-frame-hooks)
+(add-hook 'after-make-frame-functions
+          'run-after-make-frame-hooks)
+
 (add-hook 'after-init-hook
-(lambda ()
-(run-after-make-frame-hooks (selected-frame))))
+          (lambda ()
+            (run-after-make-frame-hooks (selected-frame))))
 
 
-;;(load-file "~/.emacs.d/load/cache.el")
-(load-file "~/.emacs.d/load/colors.el")
-(load-file "~/.emacs.d/load/modes.el")
-(load-file "~/.emacs.d/load/prefs.el")
-(load-file "~/.emacs.d/load/typing-pairs.el")
-(load-file "~/.emacs.d/load/completion.el")
-(load-file "~/.emacs.d/load/misc.el")
-(load-file "~/.emacs.d/load/ido-smex.el")  ;; Should be loaded last.
+;; ***************************************************************************
+;; Automatically recompile the emacs init file on buffer-save or exit
+;; ------------------------------------------------------------------
+;;
 
-;; Recompile the init file (~/.emacs) on buffer-save or emacs-exit.
 (defun byte-compile-user-init-file ()
   (let ((byte-compile-warnings '(unresolved)))
-        ;; in case compilation fails, don't leave the old .elc around:
-        (when (file-exists-p (concat user-init-file ".elc"))
-          (delete-file (concat user-init-file ".elc")))
-        (byte-compile-file user-init-file)
-        ;; (message "%s compiled" user-init-file)
-        ))
+    ;; in case compilation fails, don't leave the old .elc around:
+    (when (file-exists-p (concat user-init-file ".elc"))
+      (delete-file (concat user-init-file ".elc")))
+    (byte-compile-file user-init-file)
+    ;; (message "%s compiled" user-init-file)
+    ))
+
 (defun my-emacs-lisp-mode-hook ()
   (when (equal buffer-file-name user-init-file)
     (add-hook 'after-save-hook 'byte-compile-user-init-file t t)))
+
 ;; (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'my-emacs-lisp-mode-hook)
 (add-hook 'kill-emacs-hook 'byte-compile-user-init-file t t)
